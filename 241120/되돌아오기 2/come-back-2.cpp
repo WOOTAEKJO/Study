de97b56ee2d3aef278b7f086d670835def781e0f
WOOTAEKJO <114208566+WOOTAEKJO@users.noreply.github.com>
2024-11-20 23:05:59 +0900

    Title: 되돌아오기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241120/되돌아오기 2/come-back-2.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+int Rotate(char c, int dir)
+{
+    if(c == 'R')
+        return (dir + 1) % 4;
+    else if(c == 'L')
+        return (dir - 1 + 4) % 4;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int iCount(0);
+
+    int dx[4] = {1,0,-1,0};
+    int dy[4] = {0,-1,0,1};
+
+    int dir = 3;
+
+    int point[2] = {};
+
+    string str;
+
+    cin >> str;
+
+    for(auto c : str)
+    {
+        if(c == 'F')
+        {
+            point[0] += dx[dir];
+            point[1] += dy[dir];
+        }else
+            dir = Rotate(c,dir);
+
+        iCount++;
+
+        if(point[0] == 0 && point[1] == 0)
+            break;
+    }
+
+    if(iCount > str.size())
+        cout << -1;
+    else   
+        cout << iCount;
+
+    return 0;
+}
